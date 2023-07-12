34816affaa55f95d4a46d6e37da3f885b550d2f5
Jordan Herrera Vera <jordan.herreravera@gmail.com>
2023-07-11 19:37:50 -0600

    added full website code

---
+++ javascript/main.js
@@ -0,0 +1,29 @@
+const container = document.querySelector('.chart__container')
+
+let values = []
+// use fetch instead import for compatibility isues with firefox
+fetch("../data.json")
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(data => values.push(data.amount))
+        let maxValue = Math.max(...values)
+        const chartsContentHTML = data.map(chart  => `
+
+        <div class="chart__bar-container">
+        <div class="chart__bar--day">${chart.day}</div>
+          
+            <div class="chart__bar" 
+            style="height:${(chart.amount * 100) / maxValue}%; 
+            background-color: ${(chart.amount % maxValue == 0)  ? "var(--Cyan)" :  "var(--Soft-red)"};">
+            <div class="chart__bar--label">$${chart.amount}</div>
+            </div>
+            
+        
+          </div>
+        `).join('')
+        container.innerHTML = chartsContentHTML;
+    })
+    .catch(error => {
+        console.error('Error loading JSON file', error)
+    })
+
